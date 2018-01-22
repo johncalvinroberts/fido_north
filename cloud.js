7a0b03a8cbf2e51a711b9c4e19cdebb9201fd420
John Roberts <john.calvin.roberts@gmail.com>
2018-01-22 13:25:59 +0800

    testing ci again

--- cloud.js
+++ cloud.js
@@ -6,9 +6,11 @@ AV.Cloud.beforeSave('_User', function(request) {
   var adopterCredentials = request.object.get('personalNote') && request.object.get('wechatId');
   var rescuerCredentials = request.object.get('id_number') && request.object.get('id_type')
   if(adopterCredentials && !rescuerCredentials) {
+    console.log('user verified as adopter')
     request.object.set('adoptVerified', true);
   }
   if (adopterCredentials && rescuerCredentials) {
+    console.log('user verified as rescuer')
     request.object.set('adoptVerified', true);
     request.object.set('is_rescuer', true);
   }
