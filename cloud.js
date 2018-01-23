933fdd4cde34f1dd2ed0a96c9f1c1f3d8bc84d0b
John Roberts <john.calvin.roberts@gmail.com>
2018-01-23 11:01:18 +0800

    remove before save

--- cloud.js
+++ cloud.js
@@ -1,17 +1,12 @@
 var AV = require('leanengine');
 
 
-AV.Cloud.beforeSave('_User', function(request) {
-  console.log('user save hook')
-})
-
 // update the users verification and role depending on credentials given
 AV.Cloud.beforeUpdate('_User', function(request) {
-  console.log('user update hook')
+  console.log('user update hook');
   var adopterCredentials = request.object.get('personalNote') && request.object.get('wechatId');
   var rescuerCredentials = request.object.get('id_number') && request.object.get('id_type')
   if(adopterCredentials && !rescuerCredentials) {
-    console.log('verify as adopter');
     request.object.set('adoptVerified', true);
     request.object.save().then(function(obj) {
       console.log('successfully updated adoptVerified');
