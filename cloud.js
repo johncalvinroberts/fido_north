212f6556c7c6d8c5f15b08ab1b67a4e0a9899d6a
John Roberts <john.calvin.roberts@gmail.com>
2018-01-23 14:44:32 +0800

    props in camel case

--- cloud.js
+++ cloud.js
@@ -7,7 +7,7 @@ AV.Cloud.afterUpdate('_User', function(request) {
   console.log('user update hook');
   return new Promise(function(resolve, reject){
     var adopterCredentials = request.object.get('personalNote') && request.object.get('wechatId') && request.object.get('age');
-    var rescuerCredentials = request.object.get('id_number') && request.object.get('id_type') && request.object.get('age');
+    var rescuerCredentials = request.object.get('idNumber') && request.object.get('idType') && request.object.get('age');
     if(adopterCredentials && !rescuerCredentials) {
       console.log('hey')
       request.object.set('adoptVerified', true);
