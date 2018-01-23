19afdc24744fbab796db42c8143427d5ff425f5e
John Roberts <john.calvin.roberts@gmail.com>
2018-01-23 12:20:32 +0800

    switch to promise again omfg

--- cloud.js
+++ cloud.js
@@ -2,26 +2,34 @@ var AV = require('leanengine');
 
 
 // update the users verification and role depending on credentials given
-AV.Cloud.beforeUpdate('_User', function(request, response) {
+
+AV.Cloud.afterUpdate('_User', function(request) {
   console.log('user update hook');
-  var adopterCredentials = request.object.get('personalNote') && request.object.get('wechatId') && request.object.get('age');
-  var rescuerCredentials = request.object.get('id_number') && request.object.get('id_type') && request.object.get('age');
-  if(adopterCredentials && !rescuerCredentials) {
-    request.object.set('adoptVerified', true);
-    request.object.save().then(function(user) {
-      console.log('successfully updated adopterVerified');
-      return response.success(user);
-    });
-  }
-  if (adopterCredentials && rescuerCredentials) {
-    console.log('user verified as rescuer');
-    request.object.set('adoptVerified', true);
-    request.object.set('is_rescuer', true);
-    request.object.save().then(function(user) {
-      console.log('successfully updated is_rescuer');
-      return response.success(user);
-    });
-  }
+  return new Promise(function(resolve, reject){
+    var adopterCredentials = request.object.get('personalNote') && request.object.get('wechatId') && request.object.get('age');
+    var rescuerCredentials = request.object.get('id_number') && request.object.get('id_type') && request.object.get('age');
+    if(adopterCredentials && !rescuerCredentials) {
+      console.log('hey')
+      request.object.set('adoptVerified', true);
+      request.object
+        .save()
+        .then(function(user) {
+        console.log('successfully updated adopterVerified');
+        resolve(user);
+      })
+        .catch(err => reject(err));
+    }
+    if (adopterCredentials && rescuerCredentials) {
+      console.log('user verified as rescuer');
+      request.object.set('adoptVerified', true);
+      request.object.set('is_rescuer', true);
+      request.object
+        .save()
+        .then(function(user) {
+        console.log('successfully updated is_rescuer');
+        resolve(user);
+      })
+        .catch(err => reject(err));
+      }
+  });
 });
-
-
