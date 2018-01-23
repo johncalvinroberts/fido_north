ce56fdc36760d79e0a73519fd268ec2fb7b98fe0
John Roberts <john.calvin.roberts@gmail.com>
2018-01-23 12:00:26 +0800

    try returning a successful response

--- cloud.js
+++ cloud.js
@@ -2,24 +2,24 @@ var AV = require('leanengine');
 
 
 // update the users verification and role depending on credentials given
-AV.Cloud.beforeUpdate('_User', function(request) {
+AV.Cloud.beforeUpdate('_User', function(request, response) {
   console.log('user update hook');
-  var adopterCredentials = request.object.get('personalNote') && request.object.get('wechatId');
-  var rescuerCredentials = request.object.get('id_number') && request.object.get('id_type')
+  var adopterCredentials = request.object.get('personalNote') && request.object.get('wechatId') && request.object.get('age');
+  var rescuerCredentials = request.object.get('id_number') && request.object.get('id_type') && request.object.get('age');
   if(adopterCredentials && !rescuerCredentials) {
     request.object.set('adoptVerified', true);
-    request.object.save().then(function(obj) {
-      console.log('successfully updated adoptVerified');
-      return request;
+    request.object.save().then(function(user) {
+      console.log('successfully updated adopterVerified');
+      return response.success(user);
     });
   }
   if (adopterCredentials && rescuerCredentials) {
     console.log('user verified as rescuer');
     request.object.set('adoptVerified', true);
     request.object.set('is_rescuer', true);
-    request.object.save().then(function(obj) {
+    request.object.save().then(function(user) {
       console.log('successfully updated is_rescuer');
-      return request;
+      return response.success(user);
     });
   }
 });
