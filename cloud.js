3afd46d19876c25984dda538c711ddd59d63d375
John Roberts <john.calvin.roberts@gmail.com>
2018-01-27 13:06:59 +0800

    remove hooks

--- cloud.js
+++ cloud.js
@@ -3,35 +3,20 @@ var AV = require('leanengine');
 
 // update the users verification and role depending on credentials given
 
-AV.Cloud.afterUpdate('_User', function(request) {
-  console.log('user update hook');
-  return new Promise(function(resolve, reject){
-    var adopterCredentials = request.object.get('personalNote') && request.object.get('wxUsername') && request.object.get('age');
-    var rescuerCredentials = request.object.get('idNumber') && request.object.get('idType') && request.object.get('age');
-    console.log(adopterCredentials)
-    console.log(rescuerCredentials)
-    if(adopterCredentials && !rescuerCredentials) {
-      console.log('hey')
-      request.object.set('adoptVerified', true);
-      request.object
-        .save()
-        .then(function(user) {
-        console.log('successfully updated adopterVerified');
-        resolve(user);
-      })
-        .catch(err => reject(err));
-    }
-    if (adopterCredentials && rescuerCredentials) {
-      console.log('user verified as rescuer');
-      request.object.set('adoptVerified', true);
-      request.object.set('rescueVerified', true);
-      request.object
-        .save()
-        .then(function(user) {
-        console.log('successfully updated is_rescuer');
-        resolve(user);
-      })
-        .catch(err => reject(err));
-      }
-  });
-});
+// AV.Cloud.afterUpdate('_User', function(request) {
+//   console.log('user update hook');
+//   return new Promise(function(resolve, reject){
+//     var adopterCredentials = request.object.get('personalNote') && request.object.get('wxUsername') && request.object.get('age');
+//     var rescuerCredentials = request.object.get('idNumber') && request.object.get('idType') && request.object.get('age');
+//     if(adopterCredentials && rescuerCredentials) {
+//       request.object.set('adoptVerified', true);
+//       request.object
+//         .save()
+//         .then(function(user) {
+//         console.log('successfully updated adopterVerified');
+//         resolve(user);
+//       })
+//         .catch(err => reject(err));
+//     }
+//   });
+// });
